82643b0a9c1517864c1dd9fcaa8a725544e5343c
USER <USER@ADMIN>
2016-11-17 13:39:11 +0700

    first comit

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,15 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen extends ScreenAdapter {
+	private VersusX versusx;
+	
+	public GameScreen(VersusX versusx){
+		this.versusx = versusx;
+	}
+	
+	
+	
+}
